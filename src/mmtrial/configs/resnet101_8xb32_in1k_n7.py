3d13024920761c1d389f26faeeeb3e2e88932637
ZHANG, Meng <stall-breezes.0t@icloud.com>
2024-10-24 18:03:31 +1300

    refine n7

--- src/mmtrial/configs/resnet101_8xb32_in1k_n7.py
+++ src/mmtrial/configs/resnet101_8xb32_in1k_n7.py
@@ -14,12 +14,11 @@ N_CLASS = 7
 TOPK = (1, 2)
 N_PER_BATCH = 70
 N_PER_CLASS = 10
-N_EPOCH = 600
-N_EPOCH_FREEZE = 200
+N_EP = 30
+N_EP_FREEZE = 10
 N_WORKERS = 10
 DATA_ROOT = "data/category"
 RESIZE_SCALE = (224, 224)
-
 METAINFO = {
     "classes": (
         "bump",
@@ -31,6 +30,36 @@ METAINFO = {
         "uneven",
     ),
 }
+
+PIPELINE_TR = (
+    dict(type="LoadImageFromFile"),
+    dict(scale=224, crop_ratio_range=(0.9, 1.0), type="RandomResizedCrop"),
+    # dict(keep_ratio=True, scale=RESIZE_SCALE, type="Resize"),
+    dict(type="RandTx"),  # all-in-one custom transform
+    # dict(direction=["horizontal", "vertical"], prob=0.5, type="RandomFlip"),
+    dict(type="PackInputs"),
+)
+PIPELINE_VA = (
+    dict(type="LoadImageFromFile"),
+    dict(edge="short", scale=256, type="ResizeEdge"),
+    dict(crop_size=224, type="CenterCrop"),
+    dict(type="PackInputs"),
+)
+
+DATASET_TR_CFG = (
+    ("data_root", DATA_ROOT),
+    ("pipeline", list(PIPELINE_TR)),
+    ("split", "train"),
+    ("metainfo", METAINFO),
+    ("type", "ImageNet"),
+)
+DATASET_VA_CFG = (
+    ("data_root", DATA_ROOT),
+    ("pipeline", list(PIPELINE_VA)),
+    ("split", "val"),
+    ("metainfo", METAINFO),
+    ("type", "ImageNet"),
+)
 
 auto_scale_lr = dict(base_batch_size=256)
 data_preprocessor = dict(
@@ -59,7 +88,7 @@ log_level = "INFO"
 model = dict(
     backbone=dict(
         depth=101,
-        frozen_stages=-1,  # -1 means all layers are trainable
+        frozen_stages=1,
         init_cfg=dict(checkpoint="torchvision://resnet101", type="Pretrained"),
         norm_cfg=dict(requires_grad=True, type="BN"),  # TODO: GN
         norm_eval=True,
@@ -80,18 +109,18 @@ model = dict(
 )
 
 optim_wrapper = dict(
-    optimizer=dict(lr=0.001, momentum=0.9, type="SGD", weight_decay=0.0001),
+    optimizer=dict(lr=0.0001, momentum=0.9, type="SGD", weight_decay=0.0001),
     type="OptimWrapper",
 )
 param_scheduler = [
     # ========== LR warm-up ==========
-    # After 100 epochs, the learning rate will reach the base learning rate
-    # (0.001) from the initial one (0.0001)
+    # After 5 epochs, the learning rate will reach the base learning rate
+    # (0.0001) from the initial one (0.00001)
     dict(
         begin=0,
         by_epoch=True,
-        end=100,
-        start_factor=0.01,
+        end=5,
+        start_factor=0.1,
         end_factor=1.0,
         type="LinearLR",
     ),
@@ -99,61 +128,37 @@ param_scheduler = [
     # The learning rate will be decayed by a factor of 0.2 each time AFTER
     # the specified epoch milestones.
     dict(
-        begin=100,
+        begin=0,
         by_epoch=True,
-        end=500,
-        gamma=0.2,
-        milestones=[120, 220, 320, 420],
+        end=N_EP,
+        gamma=0.5,
+        milestones=[8, 13, 18, 23],
         type="MultiStepLR",
     ),
 ]
 
-# # We do NOT freeze the backbone
-# custom_hooks = [
-#     dict(type="UnfreezeMMPretrainHook", unfreeze_epoch=N_EPOCH_FREEZE),
-# ]
+# Unfreeze the backbone after some epochs
+custom_hooks = [
+    dict(type="UnfreezeMMPretrainHook", unfreeze_epoch=N_EP_FREEZE),
+]
 randomness = dict(deterministic=False, seed=None)
 resume = False
 
 test_cfg = dict()
-test_pipeline = [
-    dict(type="LoadImageFromFile"),
-    dict(edge="short", scale=256, type="ResizeEdge"),
-    dict(crop_size=224, type="CenterCrop"),
-    dict(type="PackInputs"),
-]
+test_pipeline = list(PIPELINE_VA)
 test_dataloader = dict(
     batch_size=N_PER_BATCH,
-    dataset=dict(
-        data_root=DATA_ROOT,
-        pipeline=test_pipeline,
-        split="val",
-        metainfo=METAINFO,
-        type="ImageNet",
-    ),
+    dataset=dict(DATASET_VA_CFG),
     num_workers=N_WORKERS,
     sampler=dict(type="BalanceSampler", num_per_class=N_PER_CLASS),
 )
 test_evaluator = dict(topk=TOPK, type="Accuracy")
 
-train_cfg = dict(by_epoch=True, max_epochs=N_EPOCH, val_interval=1)
-train_pipeline = [
-    dict(type="LoadImageFromFile"),
-    dict(scale=224, crop_ratio_range=(0.9, 1.0), type="RandomResizedCrop"),
-    # dict(keep_ratio=True, scale=RESIZE_SCALE, type="Resize"),
-    dict(type="RandTx"),  # all-in-one custom transform
-    # dict(direction=["horizontal", "vertical"], prob=0.5, type="RandomFlip"),
-    dict(type="PackInputs"),
-]
+train_cfg = dict(by_epoch=True, max_epochs=N_EP, val_interval=1)
+train_pipeline = list(PIPELINE_TR)
 train_dataloader = dict(
     batch_size=N_PER_BATCH,
-    dataset=dict(
-        data_root=DATA_ROOT,
-        pipeline=train_pipeline,
-        split="train",
-        metainfo=METAINFO,
-        type="ImageNet",
-    ),
+    dataset=dict(DATASET_TR_CFG),
     num_workers=N_WORKERS,
     sampler=dict(type="BalanceSampler", num_per_class=N_PER_CLASS),
 )
@@ -161,13 +166,7 @@ train_dataloader = dict(
 val_cfg = dict()
 val_dataloader = dict(
     batch_size=N_PER_BATCH,
-    dataset=dict(
-        data_root=DATA_ROOT,
-        pipeline=test_pipeline,
-        split="val",
-        metainfo=METAINFO,
-        type="ImageNet",
-    ),
+    dataset=dict(DATASET_VA_CFG),
     num_workers=N_WORKERS,
     sampler=dict(type="BalanceSampler", num_per_class=N_PER_CLASS),
 )
@@ -176,7 +175,5 @@ val_evaluator = dict(topk=TOPK, type="Accuracy")
 vis_backends = [dict(type="LocalVisBackend")]
 visualizer = dict(
     type="UniversalVisualizer",
-    vis_backends=[
-        dict(type="LocalVisBackend"),
-    ],
+    vis_backends=vis_backends,
 )
